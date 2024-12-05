af6a90ac524fa14648ed0d4a0dd20919707a5e02
Fabian Mühlenbein <fabian.muehlenbein@fino.digital>
2024-12-05 07:18:57 +0100

    Move resources back in place but unstaged

--- src/main/kotlin/day_01/Util.kt
+++ src/main/kotlin/day_01/Util.kt
@@ -2,7 +2,7 @@ package gringo.fabi.aoc.day_01
 
 import java.io.InputStream
 
-const val RESOURCE = "puzzles/01_1_input.txt"
+const val RESOURCE = "01_1_input.txt"
 
 fun InputStream.toPairOfLists(): Pair<List<Int>, List<Int>> = bufferedReader().useLines { lines ->
     val first = mutableListOf<Int>()
